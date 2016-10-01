34778c4a94dc564a7af3b64a068691e255f86b3e
heiga <phillip2@ualberta.ca>
2016-10-01 12:25:41 -0600

    windows commit 1
    testing windows commit

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Habit.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by phillip2 on 9/28/16.
+ */
+public class Habit {
+    private String name;
+    private String notes;
+    private Date habitDate;
+    private ArrayList<Date> completedDates;
+
+}
