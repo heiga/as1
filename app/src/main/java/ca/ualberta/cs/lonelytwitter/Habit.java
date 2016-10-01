7600472139866e93b1e383d94e5913e307404eb2
heiga <phillip2@ualberta.ca>
2016-10-01 13:23:02 -0600

    habit class 1
    finished draft 1 for habit class

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Habit.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Habit.java
@@ -2,6 +2,7 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by phillip2 on 9/28/16.
@@ -11,5 +12,31 @@ public class Habit {
     private String notes;
     private Date habitDate;
     private ArrayList<Date> completedDates;
+    private List<String> daysOfWeek;
+    private Integer habitCount;
+
+    public Habit(String habitName, String habitNotes,
+                      Date dateOfHabit, List<String> weekDays) {
+        this.name = habitName;
+        this.notes = habitNotes;
+        this.habitDate = dateOfHabit;
+        this.daysOfWeek = weekDays;
+        habitCount = 0;
+    }
+
+    public void habitCompletion(Date completionDate) {
+        completedDates.add(completionDate);
+        habitCount += 1;
+    }
+
+    public void habitRemoval(Date completionDate) {
+        completedDates.remove(completionDate);
+        habitCount -= 1;
+    }
+
+    public Integer viewHabitCount() {
+        return habitCount;
+    }
+
 
 }
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/HabitList.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Phillip Hoang on 2016-10-01.
+ */
+public class HabitList {
+    private ArrayList<Habit> habitList;
+
+    public HabitList(){
+
+    }
+}
