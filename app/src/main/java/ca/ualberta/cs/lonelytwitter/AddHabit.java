d106a2fb27e083d5268ee30532748794d35afa4b
heiga <phillip2@ualberta.ca>
2016-10-02 12:36:52 -0600

    date picker done
    routine push

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/AddHabit.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/AddHabit.java
@@ -1,12 +1,18 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.view.Menu;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.util.Calendar;
+
+// All DatePicker code from http://www.tutorialspoint.com/android/android_datepicker_control.htm
+// Usage from this site is freely allowed for educational purposes
 
 public class AddHabit extends Activity {
 
@@ -19,5 +25,43 @@ public class AddHabit extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+
+        dateView = (TextView) findViewById(R.id.dateDisplay);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 }
