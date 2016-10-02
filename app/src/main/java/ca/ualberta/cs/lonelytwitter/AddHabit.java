c410c9a198021b827711b94522c994c910155b66
heiga <phillip2@ualberta.ca>
2016-10-01 22:27:49 -0600

    addhabit layout done
    routine push

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/AddHabit.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/AddHabit.java
@@ -3,8 +3,17 @@ package ca.ualberta.cs.lonelytwitter;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddHabit extends Activity {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
